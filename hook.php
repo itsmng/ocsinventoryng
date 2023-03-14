10284d62c9dab74866748cf798714e516e9fe230
Charlene Auger <charlene.auger@factorfx.com>
2023-03-14 17:29:41 +0100

    Add update to 1.7.6

--- hook.php
+++ hook.php
@@ -553,6 +553,19 @@ function plugin_ocsinventoryng_install() {
          $DB->runFile(GLPI_ROOT . "/plugins/ocsinventoryng/install/mysql/1.7.5-update.sql");
       }/*1.7.5*/
 
+      /******************* Migration 1.7.6 *******************/
+      // Update 1.7.6
+      if ($DB->tableExists("glpi_plugin_ocsinventoryng_snmpocslinks")
+          && !$DB->fieldExists('glpi_plugin_ocsinventoryng_snmpocslinks', 'ocstype')) {
+
+         $query = "ALTER TABLE `glpi_plugin_ocsinventoryng_snmpocslinks` ADD `ocstype` INT(11) DEFAULT NULL;";
+         $DB->queryOrDie($query, "1.7.6 update table glpi_plugin_ocsinventoryng_snmpocslinks");
+      }
+      
+      if ($DB->tableExists("glpi_plugin_ocsinventoryng_ipdiscoversnmpreconciliation")) {
+         $DB->runFile(GLPI_ROOT . "/plugins/ocsinventoryng/install/mysql/1.7.6-update.sql");
+      }/*1.7.6*/
+
       $migration->executeMigration();
 
    }
